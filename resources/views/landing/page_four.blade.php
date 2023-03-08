98387c84ea02cdb09d63ec96c65278753a304378
candrabudi <candrasachio@gmail.com>
2023-03-08 10:45:17 +0700

    Fixing type input and styling

--- resources/views/landing/page_four.blade.php
+++ resources/views/landing/page_four.blade.php
@@ -5,28 +5,28 @@
     </div>
     <div class="row g-3">
         <div class="col-sm-6">
-            <label class="form-label" for="ijazahSd">Upload Ijazah SD</label>
-            <input type="file" name="ijazahSd" id="ijazahSd" class="form-control" />
+            <label class="form-label" for="ijazahSd">Upload Ijazah SD (halaman depan dan belakang), Format: Image/Pdf*</label>
+            <input type="file" name="ijazahSd" id="ijazahSd" class="form-control" accept="image/*,.pdf"/>
         </div>
         <div class="col-sm-6">
-            <label class="form-label" for="aktaKelahiran">Upload Akta Kelahiran</label>
-            <input type="file" name="aktaKelahiran" id="aktaKelahiran" class="form-control" />
+            <label class="form-label" for="aktaKelahiran">Upload Akta Kelahiran, Format: Image/Pdf*</label>
+            <input type="file" name="aktaKelahiran" id="aktaKelahiran" class="form-control" accept="image/*,.pdf" />
         </div>
         <div class="col-sm-6">
-            <label class="form-label" for="kartuKeluarga">Upload Kartu Keluarga</label>
-            <input type="file" name="kartuKeluarga" id="kartuKeluarga" class="form-control" />
+            <label class="form-label" for="kartuKeluarga">Upload Kartu Keluarga, Format: Image/Pdf*</label>
+            <input type="file" name="kartuKeluarga" id="kartuKeluarga" class="form-control" accept="image/*,.pdf" />
         </div>
         <div class="col-sm-6">
-            <label class="form-label" for="pasFoto">Pas Foto</label>
-            <input type="file" name="pasFoto" id="pasFoto" class="form-control" />
+            <label class="form-label" for="pasFoto">Pas Foto, Format: Image*</label>
+            <input type="file" name="pasFoto" id="pasFoto" class="form-control" accept="image/png, image/jpg, image/jpeg" />
         </div>
         <div class="col-sm-6">
-            <label class="form-label" for="tandaTangan">Tanda Tangan</label>
-            <input type="file" name="tandaTangan" id="tandaTangan" class="form-control" />
+            <label class="form-label" for="tandaTangan">Tanda Tangan, Format: Image/Pdf*</label>
+            <input type="file" name="tandaTangan" id="tandaTangan" accept="image/*,.pdf" class="form-control" />
         </div>
         <div class="col-sm-6">
-            <label class="form-label" for="softCopyRaportSmp">Softcopy Raport SMP</label>
-            <input type="file" name="softCopyRaportSmp" id="softCopyRaportSmp" class="form-control" />
+            <label class="form-label" for="softCopyRaportSmp">Softcopy Raport SMP (dari halaman depan), Format: Image/Pdf*</label>
+            <input type="file" name="softCopyRaportSmp" id="softCopyRaportSmp" accept="image/*,.pdf" class="form-control" />
         </div>
         <div class="col-12 d-flex justify-content-between">
             <a class="btn btn-label-secondary btn-prev" style="color: #AEAEAE;">
